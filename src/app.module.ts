956ada6c322f0768a0b8036cc2d914430c931280
Ndizihiwe Benny Chrispin <ndizibaidu23@gmail.com>
2026-02-28 17:17:47 +0200

    fix: wire up full app in main.ts and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { validate } from './config/env.validation';
+import { RedisModule } from './common/redis/redis.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TrafficModule } from './traffic/traffic.module';
+import { ParkingModule } from './parking/parking.module';
+import { WeatherModule } from './weather/weather.module';
+import { User } from './database/entities/user.entity';
+import { Vehicle } from './database/entities/vehicle.entity';
+import { TrafficLocation } from './database/entities/traffic-location.entity';
+import { ParkingSpot } from './database/entities/parking-spot.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User, Vehicle, TrafficLocation, ParkingSpot],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        ssl:
+          configService.get<string>('NODE_ENV') === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+      }),
+      inject: [ConfigService],
+    }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 20 }]),
+    RedisModule,
+    AuthModule,
+    UserModule,
+    TrafficModule,
+    ParkingModule,
+    WeatherModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenWay API')
+    .setDescription('Kigali city navigation backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
