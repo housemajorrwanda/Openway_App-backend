69c3316604e5a4585e532249a4817f78c47589cf
Ndizihiwe Benny Chrispin <ndizibaidu23@gmail.com>
2026-03-07 19:47:09 +0200

    feat: update login DTO examples for email and password

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'jean@example.com' })
+  @ApiProperty({ example: 'admin@openway.rw' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'Password1!' })
+  @ApiProperty({ example: 'Password123?' })
   @IsString()
   @MinLength(8)
   password: string;
